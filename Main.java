90895946e49d9128fc55f36b99bc996849da10a2
IlhamArifinnn <arifinn2208@gmail.com>
2024-09-28 19:39:17 +0700

    praktikum1 pdpl

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Ilham Arifin";
+        String NIM = "0110223244";
+        String prodi = "Teknik Informatika";
+        String kampus = "STT Terpadu Nurul Fikri";
+        String alamat = "Depok, Jawa Barat";
+
+        System.out.println("Nama: " + name);
+        System.out.println("NIM: " + NIM);
+        System.out.println("Prodi: " + prodi);
+        System.out.println("Kampus: " + kampus);
+        System.out.println("Alamat: " + alamat);
+
+    }
+}
