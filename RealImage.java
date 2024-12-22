b825ea1db0e895128f474331a1de5a4b4b47997d
IlhamArifinnn <arifinn2208@gmail.com>
2024-12-22 19:15:28 +0700

    upload praktikum13: Proxy

---
+++ RealImage.java
@@ -0,0 +1,20 @@
+
+public class RealImage implements Image {
+
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName) {
+        // Simulate loading from disk
+        System.out.println("Loading " + fileName);
+    }
+}
